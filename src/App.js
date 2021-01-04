f3e75c782874c5d51956b7079f770c346065b97a
Plofland <ploflan@gmail.com>
2021-01-04 18:26:21 -0500

    App.js -> fixed the SignUp component import statement, SignUp.js -> got a basic signup structure

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { createContext } from 'react';
 import './App.css';
-import {BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom';
-
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from 'react-router-dom';
 
 import Home from './components/Home';
 import Nav from './components/Nav';
 import Developers from './components/Developers';
 import MeetTheTeam from './components/MeetTheTeam';
-import Signup from './components/MeetTheTeam';
+import Signup from './components/Signup';
 import Login from './components/Login';
 
 import PrivateRoute from './utils/PrivateRoute';
@@ -15,30 +19,27 @@ import IssuesList from './components/IssuesList';
 import AddIssue from './components/AddIssue';
 import CreateProfile from './components/CreateProfile';
 
-export const LoginContext = createContext()
+export const LoginContext = createContext();
 
 function App() {
   return (
     <div>
       <Router>
-        <Nav/>
-        <Route exact path='/'>
-          <Redirect to={{ pathname: '/home'}}/>
+        <Nav />
+        <Route exact path="/">
+          <Redirect to={{ pathname: '/home' }} />
         </Route>
-        <Route path='/home' component={Home}/>
-        <Route path='/meettheteam' component={MeetTheTeam}/>
-        <Route path='/developers' component={Developers}/>
-        <Route path='/login' component={Login}/>
-        <Route path='/signup' component={Signup}/>
-        <Route path='/addissue' component={AddIssue}/>
-        <Route path='/issueslist' component={IssuesList}/>
-        <Route path='/createprofile' component={CreateProfile}/>
+        <Route path="/home" component={Home} />
+        <Route path="/meettheteam" component={MeetTheTeam} />
+        <Route path="/developers" component={Developers} />
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={Signup} />
+        <Route path="/addissue" component={AddIssue} />
+        <Route path="/issueslist" component={IssuesList} />
+        <Route path="/createprofile" component={CreateProfile} />
       </Router>
     </div>
   );
 }
 
 export default App;
-
-
-
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,12 +1,36 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const Signup = () => {
+const StyledSignUp = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  border: 2px solid black;
+`;
 
-    return (
-<>
+function Signup() {
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
 
-</>
-    )
+  // const handleChange = (event) =>{
+  //   const { name }
+  // }
+
+  return (
+    <StyledSignUp>
+      <form onSubmit={onSubmit}>
+        <input type="email" name="email" placeholder="Email"></input>
+        <input type="text" name="username" placeholder="Username"></input>
+        <input type="password" placeholder="Password"></input>
+        <input
+          type="submit"
+          // disabled={disabled}
+          // onChange={handleChange}
+        ></input>
+      </form>
+    </StyledSignUp>
+  );
 }
 
-export default Signup
+export default Signup;
