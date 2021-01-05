80893a112295168c009fa4348626f1aeb75b77b2
Plofland <ploflan@gmail.com>
2021-01-04 19:23:12 -0500

    added some styling to SignUp.js

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -3,9 +3,39 @@ import styled from 'styled-components';
 
 const StyledSignUp = styled.div`
   display: flex;
-  flex-direction: row;
-  justify-content: center;
-  border: 2px solid black;
+  margin: 0 auto;
+  width: 50%;
+  /* color: white; */
+  border: 2px solid black; /* Don't forget to delete this */
+
+  body {
+    background-color: #242943;
+  }
+
+  form {
+    display: flex;
+    flex-direction: column;
+    width: 70%;
+    margin: 10% auto;
+    background-color: #8d82c4;
+    border-radius: 2%;
+    border: 1px solid blue; /* Don't forget to delete this */
+
+    /* box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px,
+      rgba(0, 0, 0, 0.3) 0px -12px 30px, rgba(0, 0, 0, 0.3) 0px 4px 6px,
+      rgba(0, 0, 0, 0.35) 0px 12px 13px, rgba(0, 0, 0, 0.2) 0px -3px 5px; */
+
+    /* box-shadow: rgba(136, 165, 191, 0.2) 6px 2px 16px 0px,
+      rgba(255, 255, 255, 0.5) -6px -2px 16px 0px; */
+
+    box-shadow: rgba(0, 0, 0, 0.15) 0px 15px 25px,
+      rgba(0, 0, 0, 0.05) 0px 5px 10px;
+  }
+
+  input {
+    margin: 3% auto;
+    width: 80%;
+  }
 `;
 
 function Signup() {
