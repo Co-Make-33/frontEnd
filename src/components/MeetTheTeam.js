c42f6eca13e061b66ed46434289bf938a512dd0a
Plofland <ploflan@gmail.com>
2021-01-05 14:10:06 -0500

    Completely changing up my axios call loopto a useEffect loop

--- src/components/MeetTheTeam.js
+++ src/components/MeetTheTeam.js
@@ -1,12 +1,23 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import styled from 'styled-components';
+import styled, { createGlobalStyle } from 'styled-components';
 
+const MeetTheTeamGlobal = createGlobalStyle`
+* {
+    color:white;
+}
+body {
+    background-color:#242943;
+}
+`;
+
+//once the data is being displayed for each teamMember, begin styling the github profile cards
 const styledTeam = styled.div`
   display: flex;
 `;
 
 function MeetTheTeam() {
+  //array of github usernames to be plugged into the axios call loop
   const teamMembers = [
     'aprilissy',
     'ruizaj13',
@@ -15,43 +26,47 @@ function MeetTheTeam() {
     'emilyr027'
   ];
 
-  //   const [teamState, setTeamState] = useState([]);
+  //the state that will hold all the info from each teamMember loop iteration
+  //once it is full of each team member's data, it is to be used in a loop in the return statement to create each team member card
+  const [teamState, setTeamState] = useState([]);
 
-  //instead of looping, just use the axios call to set the state
+  //loop over the teamMembers array, use the axios call to set the state by adding the data object to the teamState array
   teamMembers.forEach((eachMember) => {
     axios
       .get(`https://api.github.com/users/${eachMember}`)
       .then((result) => {
-        console.log(result);
-        // setTeamState(result);
+        // console.log(result);
+        setTeamState([...teamState, result.data]);
       })
       .catch((error) => {
         console.log(error);
       });
   });
 
-  //use the loop in here to loop over the state once it is set by the axios call to create each profile card
+  console.log(teamState);
+  //once the teamState has been set with the data from each teamMember, use a loop in here to create each profile card
   return (
-    <styledTeam>
-      <div className="githubCards">
-        <img
-          src="https://avatars2.githubusercontent.com/u/11523076?v=4"
-          alt="gitHub avatar"
-        ></img>
-        <div className="profileInfo">
-          <h2>April</h2>
-          <div className="userInfo">
-            <p>Bio: placeholder</p>
-            <p>Username: placeholder</p>
-            <p>Followers: number</p>
-            <p>Following: number</p>
-            <a href="https://github.com/aprilissy">GitHub Profile</a>
+    <>
+      <MeetTheTeamGlobal />
+      <styledTeam>
+        <div className="githubCards">
+          <img
+            src="https://avatars2.githubusercontent.com/u/11523076?v=4"
+            alt="gitHub avatar"
+          ></img>
+          <div className="profileInfo">
+            <h2>April</h2>
+            <div className="userInfo">
+              <p>Bio: placeholder</p>
+              <p>Username: placeholder</p>
+              <p>Followers: number</p>
+              <p>Following: number</p>
+              <a href="https://github.com/aprilissy">GitHub Profile</a>
+            </div>
           </div>
         </div>
-        {/* <img src="https://avatars2.githubusercontent.com/u/11523076?v=4" alt="gitHub avatar" />
-      <h2>FluffyBunnyToes</h2> */}
-      </div>
-    </styledTeam>
+      </styledTeam>
+    </>
   );
 }
 
