d97c2a8bff1230053f44c6f1fa3113adb7e32be1
ruizaj13 <ruizaj13@gmail.com>
2021-01-04 16:05:07 -0800

    Upload file input added, form is now mostly functional

--- src/components/CreateProfile.js
+++ src/components/CreateProfile.js
@@ -1,5 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styled from 'styled-components';
+import axiosWithAuth from '../utils/axiosWithAuth';
+
+const initialFormState = {
+    description:'',
+    picture: ''
+}
 
 const StyledForm = styled.div`
     text-align: center;
@@ -8,30 +14,47 @@ const StyledForm = styled.div`
     a {
         cursor: pointer;
     }
+
+    input {
+       margin-left: 1%; 
+
+    }
 `
 
 
 const CreateProfile = () => {
+    const [profileData, setProfileData] = useState(initialFormState)
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        axiosWithAuth
+            .put('api/users/:id', profileData)
+    }
 
+    const handleChange = (e) => {
+        setProfileData({...profileData, [e.target.name]: e.target.value})
+        console.log(profileData)
+    }
 
     return (
         <StyledForm>
             <h3>Complete Your Profile</h3>
-            <form>
+            <form onSubmit={handleSubmit}>
                 {/* username and password will be autofilled via state*/}
-                <label> Username: <br/>
+                <label> Username: 
                     <input name='username' type='text' disabled = 'true'/> <br/>
                 </label>
                 <br/>
-                <label> Password: <br/>
+                <label> Password: 
                     <input name='password' type='password' disabled='true'/> <br/>
                 </label>
-                <br/>                
-                <a>Profile Pic upload</a> <br/>
+                <br/>
+                <label> Profile Photo:                
+                    <input onChange={handleChange} type='file' name='picture' value={profileData.picture}/> <br/>
+                </label> 
                 <br/>
                 <label> Bio: <br/>
-                    <textarea type='text' name = 'bio' rows ='20' cols='80' style={{resize: 'none'}}/>
+                    <textarea onChange={handleChange} type='text' name = 'description' value={profileData.description} rows ='10' cols='80' style={{resize: 'none'}}/>
                 </label>
                 <br/>
                 <button>Submit</button>
