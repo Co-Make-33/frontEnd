662fd7be63695f2d3e98e23b7894170c4a1dea76
Plofland <ploflan@gmail.com>
2021-01-06 12:14:33 -0500

    Adding styling to SignUp similar to MeetTheTeam

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,39 +1,46 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { createGlobalStyle } from 'styled-components';
 
-const styledSignUp = styled.div`
+const SignUpGlobal = createGlobalStyle`
+* {
+    color:white;
+}
+body {
+    background-color:#242943;
+}
+`;
+
+const Heading = styled.h2`
+  text-align: center;
+  border-bottom: 2px solid white;
+  width: 90%;
+  margin: 2% auto;
+  padding: 1% 0;
+`;
+
+const StyledSignUp = styled.div`
   display: flex;
   margin: 0 auto;
-  width: 50%;
-  /* color: white; */
-  border: 2px solid black; /* Don't forget to delete this */
+  width: 60%;
+`;
 
-  body {
-    background-color: #242943;
-  }
-
-  form {
-    display: flex;
-    flex-direction: column;
-    width: 70%;
-    margin: 10% auto;
-    background-color: #8d82c4;
-    border-radius: 2%;
-    border: 1px solid blue; /* Don't forget to delete this */
-
-    /* box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px,
-      rgba(0, 0, 0, 0.3) 0px -12px 30px, rgba(0, 0, 0, 0.3) 0px 4px 6px,
-      rgba(0, 0, 0, 0.35) 0px 12px 13px, rgba(0, 0, 0, 0.2) 0px -3px 5px; */
-
-    /* box-shadow: rgba(136, 165, 191, 0.2) 6px 2px 16px 0px,
-      rgba(255, 255, 255, 0.5) -6px -2px 16px 0px; */
-
-    box-shadow: rgba(0, 0, 0, 0.15) 0px 15px 25px,
-      rgba(0, 0, 0, 0.05) 0px 5px 10px;
-  }
+const StyledForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: end;
+  width: 70%;
+  margin: 10% auto;
+  background-color: #8d82c4;
+  border-radius: 5px;
+  box-shadow: 
+    /* top */ rgba(255, 255, 255, 0.15) 0px -6px 10px,
+    /* right */ rgba(255, 255, 255, 0.15) 6px 0px 10px,
+    /* bottom */ rgba(255, 255, 255, 0.15) 0px 6px 10px,
+    /* left */ rgba(255, 255, 255, 0.15) -6px 0px 10px;
+  border: 1px solid mintcream;
 
   input {
-    margin: 3% auto;
+    margin: 4% auto;
     width: 80%;
   }
 `;
@@ -48,18 +55,22 @@ function Signup() {
   // }
 
   return (
-    <styledSignUp>
-      <form onSubmit={onSubmit}>
-        <input type="email" name="email" placeholder="Email"></input>
-        <input type="text" name="username" placeholder="Username"></input>
-        <input type="password" placeholder="Password"></input>
-        <input
-          type="submit"
-          // disabled={disabled}
-          // onChange={handleChange}
-        ></input>
-      </form>
-    </styledSignUp>
+    <>
+      <SignUpGlobal />
+      <Heading>Sign Up</Heading>
+      <StyledSignUp>
+        <StyledForm onSubmit={onSubmit}>
+          <input type="email" name="email" placeholder="Email"></input>
+          <input type="text" name="username" placeholder="Username"></input>
+          <input type="password" placeholder="Password"></input>
+          <input
+            type="submit"
+            // disabled={disabled}
+            // onChange={handleChange}
+          ></input>
+        </StyledForm>
+      </StyledSignUp>
+    </>
   );
 }
 
