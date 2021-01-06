8a269b61f42ff101cd32c3c188e812a02879243d
Plofland <ploflan@gmail.com>
2021-01-05 19:42:11 -0500

    Got the api call working and displaying

--- src/components/MeetTheTeam.js
+++ src/components/MeetTheTeam.js
@@ -18,31 +18,45 @@ const Heading = styled.div`
 const GithubCards = styled.div`
   display: flex;
   flex-direction: column;
+  align-items: center;
+  width: 100%;
+  border: 2px solid black;
 `;
 
+//TODO: make the cards width and spacing reponsive (smaller % and wrapped when big screen, higher % and not wrapped when smaller screen)
 const SingleCard = styled.div`
   display: flex;
   flex-direction: row;
+  width: 75%;
+  margin: 3% 0;
+  border-radius: 10px;
+  box-shadow: rgba(255, 255, 255, 0.15) 0px 15px 15px,
+    rgba(255, 255, 255, 0.05) 0px 5px 10px;
+  border: 2px solid white;
 `;
 
 const Avatar = styled.img`
-  width: 25%;
+  width: 33%;
+  margin: 2%;
   /* border-radius: 50%; */
 `;
 
 const ProfileInfo = styled.div`
   width: 75%;
+  margin: 2%;
   h2 {
     font-size: 1.4rem;
+    margin-top: 0;
   }
 `;
 
 const UserInfo = styled.div`
   font-size: 0.75rem;
+  line-height: 0.5rem;
 `;
 
 //array of github usernames to be plugged into the axios call loop
-//!Why is this not
+//!Why is this not able to work for an axios call but the next one does?
 // const teamMembers = [
 //   'aprilissy',
 //   'ruizaj13',
@@ -53,6 +67,7 @@ const UserInfo = styled.div`
 
 const teamMembers = [
   { name: 'April Ashby', login: 'aprilissy' },
+  //!Should have this after the state is set in line 102 {name: 'April Ashby', login:'aprilissy', image: data.avatar_url, url: data.html_url, bio: data.bio, location: data.location}
   { name: 'Emily Ryan', login: 'emilyr027' },
   { name: 'Juan Ruiz', login: 'ruizaj13' },
   { name: 'Peter Lofland', login: 'plofland' },
@@ -67,18 +82,19 @@ function MeetTheTeam() {
   useEffect(() => {
     const devInfo = async () => {
       const developers = [...team];
-      //condense this for loop down to a for(i in team)
+      //TODO:condense this for loop down to a for(i in team), will it work?
       for (let i = 0; i < team.length; i++) {
         const { data } = await axios.get(
-          `https://api.github.com/users/${team[i]}`
+          `https://api.github.com/users/${team[i].login}`
         );
         developers[i].image = data.avatar_url;
         developers[i].url = data.html_url;
         developers[i].location = data.location;
         developers[i].bio = data.bio;
         developers[i].location = data.location;
-        developers[i].name = data.name;
-        developers[i].login = data.login;
+
+        // developers[i].name = data.name;
+        // developers[i].login = data.login;
 
         // developers[i].followers = data.followers;
         // developers[i].following = data.following;
@@ -88,6 +104,8 @@ function MeetTheTeam() {
     devInfo();
   }, []);
 
+  //   console.log(team);
+
   //once the teamState has been set with the data from each teamMember, use a loop in here to create each profile card
   return (
     <>
@@ -96,7 +114,8 @@ function MeetTheTeam() {
         <h2>Meet The Team</h2>
       </Heading>
       <GithubCards>
-        {team.map((dev, index) => {
+        {team.map((dev) => {
+          //!Why is this index here if it is never used?
           return (
             <SingleCard>
               <Avatar src={dev.image} alt="gitHub avatar" />
